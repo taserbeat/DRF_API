e8053d7b5a99bb4ba173cf9e14e0f281a1dd4856
taserbeat <taserbeat@gmail.com>
2020-11-03 16:41:11 +0900

    init

---
+++ api/views.py
@@ -0,0 +1,30 @@
+from django.shortcuts import render
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework import generics, viewsets
+from django.contrib.auth.models import User
+from .models import Task
+from .serializers import UserSerializer, TaskSerializer
+from .ownpermissions import ProfilePermission
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = (ProfilePermission,)
+
+
+class ManageUserView(generics.RetrieveAPIView):
+    serializer_class = UserSerializer
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAuthenticated,)
+
+    def get_object(self):
+        return self.request.user
+
+
+class TaskViewSet(viewsets.ModelViewSet):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAuthenticated,)
